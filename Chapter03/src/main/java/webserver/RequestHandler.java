48ebc3d1ba659a5b58e20fe260e6de17728c987f
Minseok Lee <lagender@gmail.com>
2017-02-20 15:50:35 +0900

    3.4.3.3 요구사항 3 - POST 방식으로 회원가입하기

--- Chapter03/src/main/java/webserver/RequestHandler.java
+++ Chapter03/src/main/java/webserver/RequestHandler.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 import model.User;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -41,14 +42,26 @@ public class RequestHandler extends Thread {
         		return;
         	}
         	
-        	String url = HttpRequestUtils.getUrl(line);
-     
-        	if (url.startsWith("/user/create")) {
-        		int index = url.indexOf("?");
-        		String queryString = url.substring(index+1);
-        		Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+        	String[] tokens = line.split(" ");
+
+        	int contentLength = 0;
+        	
+        	while (!line.equals("")) {
+        		log.debug("header : {}",line);
+        		line = br.readLine();
+        		if (line.contains("Content-Length")) {
+        			contentLength = getContentLength(line);
+        		}
+        	}
+        	
+        	String url = tokens[1];
+ 
+        	if (("/user/create".equals(url))) {
+        		String body = IOUtils.readData(br, contentLength);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(body);
         		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
         		log.debug("User : {}",user);
+        		
         	} else {
         		DataOutputStream dos = new DataOutputStream(out);
         		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
@@ -58,6 +71,11 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private int getContentLength(String line) {
+    	String[] headerTokens = line.split(":");
+    	return Integer.parseInt(headerTokens[1].trim());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
