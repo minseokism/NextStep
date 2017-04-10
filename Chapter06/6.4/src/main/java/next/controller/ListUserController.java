b1051f0ac19893d29704852ea7aaa2e12e923780
Minseok Lee <lagender@gmail.com>
2017-04-10 22:11:38 +0900

    Created chapter 6.4

---
+++ Chapter06/6.4/src/main/java/next/controller/ListUserController.java
@@ -0,0 +1,30 @@
+package next.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.db.DataBase;
+
+@WebServlet("/users/list")
+public class ListUserController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	if (!UserSessionUtils.isLogined(req.getSession())) {
+    		resp.sendRedirect("/users/loginForm");
+            return;
+    	}
+    	
+        req.setAttribute("users", DataBase.findAll());
+        
+        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
+        rd.forward(req, resp);
+    }
+}
