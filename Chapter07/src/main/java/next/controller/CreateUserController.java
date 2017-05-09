39d5e6adfa1b272741b36b63684a2023a20cdda7
Minseok Lee <lagender@gmail.com>
2017-05-09 19:23:03 +0900

    7.1.2 회원 목록 실습, 7.1.3 개인정보 수정 실습

--- Chapter07/src/main/java/next/controller/CreateUserController.java
+++ Chapter07/src/main/java/next/controller/CreateUserController.java
@@ -6,8 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import core.db.DataBase;
 import core.mvc.Controller;
+import next.dao.UserDao;
 import next.model.User;
 
 public class CreateUserController implements Controller {
@@ -19,7 +19,10 @@ public class CreateUserController implements Controller {
                 req.getParameter("email"));
         log.debug("User : {}", user);
 
-        DataBase.addUser(user);
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+        
         return "redirect:/";
     }
 }
+
--- Chapter07/src/main/java/next/dao/UserDao.java
+++ Chapter07/src/main/java/next/dao/UserDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,12 +37,59 @@ public class UserDao {
     }
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "UPDATE USERS SET PASSWORD=?, NAME=?, EMAIL=? WHERE USERID=?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+    	List<User> users = new ArrayList<>();
+    	Connection con = null;
+    	Statement stmt = null;
+    	ResultSet rs = null;
+    	
+    	try {
+    		con = ConnectionManager.getConnection();
+    		String sql = "SELECT userId, password, name, email FROM USERS";
+    		stmt = con.createStatement();
+    		rs = stmt.executeQuery(sql);
+
+    		if (rs.next()) {
+    			users.add( new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email")));
+    		}
+    		
+    	} finally {
+    	    if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+		}
+    	
+    	return users;
     }
 
     public User findByUserId(String userId) throws SQLException {
