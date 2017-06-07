bab069412802777092cfe34d7c2a9cfa255e8498
Minseok Lee <lagender@gmail.com>
2017-06-07 21:44:39 +0900

    7.4.9 런타임 Exception 추가 및 AutoClosable 활용한 자원 반환

--- Chapter07/src/main/java/core/jdbc/JdbcTemplate.java
+++ Chapter07/src/main/java/core/jdbc/JdbcTemplate.java
@@ -8,56 +8,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcTemplate {
-    public void update(String sql, PreparedStatementSetter pss) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+    public void update(String sql, PreparedStatementSetter pss) throws DataAccessException {
+        try (Connection con = ConnectionManager.getConnection();
+        	PreparedStatement pstmt = con.prepareStatement(sql)){
             pss.setValues(pstmt);
-
             pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
+            
+        } catch (SQLException e) {
+        	throw new DataAccessException();
         }
     }
     
 	@SuppressWarnings("rawtypes")
-	public List query(String sql, PreparedStatementSetter pss, RowMapper rowMapper) throws SQLException {
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
+	public List query(String sql, PreparedStatementSetter pss, RowMapper rowMapper) throws DataAccessException  {
 		ResultSet rs = null;
-		try {
-			con = ConnectionManager.getConnection();
-			pstmt = con.prepareStatement(sql);
+
+		try (Connection con = ConnectionManager.getConnection();
+            	PreparedStatement pstmt = con.prepareStatement(sql)){
+
 			pss.setValues(pstmt);
 
 			rs = pstmt.executeQuery();
 			
 			List<Object> result = new ArrayList<Object>();
+
 			while (rs.next()) {
 				result.add(rowMapper.mapRow(rs));
 			}
 
 			return result;
-		} finally {
-			if (rs != null) {
-				rs.close();
-			}
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (con != null) {
-				con.close();
-			}
-		}
+		} catch (SQLException e) {
+			throw new DataAccessException();
+		} 
 	}
 	
 	@SuppressWarnings("rawtypes")
