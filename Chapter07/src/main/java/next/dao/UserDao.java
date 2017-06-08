3f1c3ef5872ddde8390f8ac509f29071c99b7e1b
Minseok Lee <lagender@gmail.com>
2017-06-08 22:58:56 +0900

    7.4.12 람다를 활용한 구현

--- Chapter07/src/main/java/next/dao/UserDao.java
+++ Chapter07/src/main/java/next/dao/UserDao.java
@@ -1,11 +1,9 @@
 package next.dao;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import core.jdbc.JdbcTemplate;
-import core.jdbc.RowMapper;
 import next.model.User;
 
 public class UserDao {
@@ -25,30 +23,20 @@ public class UserDao {
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "SELECT userId, password, name, email FROM USERS";
 
-		RowMapper<User> rm = new RowMapper<User>() {
-			@Override
-			public User mapRow(ResultSet rs) throws SQLException {
-				return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-						rs.getString("email"));
-			}
-		};
-
-		return jdbcTemplate.query(sql, rm);
+		return jdbcTemplate.query(sql, (ResultSet rs) -> {
+			return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+					rs.getString("email"));
+		}); 
 	}
 
 	public User findByUserId(String userId) {
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
 
-		RowMapper<User> rm = new RowMapper<User>() {
-			@Override
-			public User mapRow(ResultSet rs) throws SQLException {
+		return jdbcTemplate.queryForObject(sql, (ResultSet rs) -> {
 				return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
 						rs.getString("email"));
-			}
-		};
-
-		return jdbcTemplate.queryForObject(sql, rm, userId);
+		}, userId);
 	}
 
 }
