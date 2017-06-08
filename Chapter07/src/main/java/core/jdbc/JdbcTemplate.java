11cbad416b278d5776d6e283eb7bb3cad8653991
Minseok Lee <lagender@gmail.com>
2017-06-08 22:55:17 +0900

    7.4.11 가변인자를 활용해  쿼리에 인자 전달하기

--- Chapter07/src/main/java/core/jdbc/JdbcTemplate.java
+++ Chapter07/src/main/java/core/jdbc/JdbcTemplate.java
@@ -8,49 +8,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcTemplate {
-    public void update(String sql, PreparedStatementSetter pss) throws DataAccessException {
-        try (Connection con = ConnectionManager.getConnection();
-        	PreparedStatement pstmt = con.prepareStatement(sql)){
-            pss.setValues(pstmt);
-            pstmt.executeUpdate();
-            
-        } catch (SQLException e) {
-        	throw new DataAccessException();
-        }
-    }
-    
-	public <T> List<T> query(String sql, PreparedStatementSetter pss, RowMapper<T> rowMapper) throws DataAccessException  {
-		ResultSet rs = null;
-
-		try (Connection con = ConnectionManager.getConnection();
-            	PreparedStatement pstmt = con.prepareStatement(sql)){
-
-			pss.setValues(pstmt);
-
-			rs = pstmt.executeQuery();
-			
-			List<T> result = new ArrayList<T>();
-
-			while (rs.next()) {
-				result.add(rowMapper.mapRow(rs));
-			}
-
-			return result;
+	public void update(String sql, PreparedStatementSetter pss) throws DataAccessException {
+		try (Connection conn = ConnectionManager.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pss.setParameters(pstmt);
+			pstmt.executeUpdate();
 		} catch (SQLException e) {
-			throw new DataAccessException();
-		} 
+			throw new DataAccessException(e);
+		}
 	}
-	
-	public <T> T queryForObject(String sql, PreparedStatementSetter pss, RowMapper<T> rowMapper) throws SQLException {
-		List<T> result = query(sql, pss, rowMapper);
-		if (result.isEmpty()) {
+
+	public <T> List<T> query(String sql, RowMapper<T> rm, PreparedStatementSetter pss) throws DataAccessException {
+		ResultSet rs = null;
+		try (Connection conn = ConnectionManager.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pss.setParameters(pstmt);
+			rs = pstmt.executeQuery();
+
+			List<T> list = new ArrayList<T>();
+			while (rs.next()) {
+				list.add(rm.mapRow(rs));
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DataAccessException(e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				throw new DataAccessException(e);
+			}
+		}
+	}
+
+	public <T> T queryForObject(String sql, RowMapper<T> rm, PreparedStatementSetter pss) {
+		List<T> list = query(sql, rm, pss);
+		if (list.isEmpty()) {
 			return null;
-		} 
-		
-		return result.get(0);
+		}
+		return list.get(0);
+	}
+
+	public void update(String sql, Object... parameters) {
+		update(sql, createPreparedStatementSetter(parameters));
 	}
 	
+	public <T> List<T> query(String sql, RowMapper<T> rm, Object... parameters) {
+		return query(sql, rm, createPreparedStatementSetter(parameters));
+	}
+	
+	public <T> T queryForObject(String sql, RowMapper<T> rm, Object... parameters) {
+		return queryForObject(sql, rm, createPreparedStatementSetter(parameters));
+	}
 
-
-    
+	private PreparedStatementSetter createPreparedStatementSetter(Object... parameters) {
+		return new PreparedStatementSetter() {
+			@Override
+			public void setParameters(PreparedStatement pstmt) throws SQLException {
+				for (int i = 0; i < parameters.length; i++) {
+					pstmt.setObject(i + 1, parameters[i]);
+				}
+			}
+		};
+	}
 }
--- Chapter07/src/main/java/next/dao/UserDao.java
+++ Chapter07/src/main/java/next/dao/UserDao.java
@@ -1,104 +1,54 @@
 package next.dao;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import core.jdbc.JdbcTemplate;
-import core.jdbc.PreparedStatementSetter;
 import core.jdbc.RowMapper;
 import next.model.User;
 
 public class UserDao {
-    public void insert(User user) throws SQLException {
-    	JdbcTemplate jdbcTemplate = new JdbcTemplate();
-    	
-    	PreparedStatementSetter pss = new PreparedStatementSetter() {
-    		@Override
-    		public void setValues(PreparedStatement pstmt) throws SQLException {
-    			pstmt.setString(1, user.getUserId());
-    			pstmt.setString(2, user.getPassword());
-    			pstmt.setString(3, user.getName());
-    			pstmt.setString(4, user.getEmail());				
-    			
-    		}
-    	};
-    	
+	public void insert(User user) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-		jdbcTemplate.update(sql, pss);
-    }
+		jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+	}
 
-    public void update(User user) throws SQLException {
-    	JdbcTemplate jdbcTemplate = new JdbcTemplate();
-    	
-    	PreparedStatementSetter pss = new PreparedStatementSetter() {
-			
-			@Override
-			public void setValues(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, user.getPassword());
-				pstmt.setString(2, user.getName());
-				pstmt.setString(3, user.getEmail());
-				pstmt.setString(4, user.getUserId());	
-				
-			}
-		};
+	public void update(User user) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "UPDATE USERS SET PASSWORD=?, NAME=?, EMAIL=? WHERE USERID=?";
-		jdbcTemplate.update(sql, pss);
-    
-    }
+		jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+	}
 
-
-	public List<User> findAll() throws SQLException {
-    	JdbcTemplate jdbcTemplate = new JdbcTemplate();
-    	
-    	PreparedStatementSetter pss = new PreparedStatementSetter() {
-			
-			@Override
-			public void setValues(PreparedStatement pstmt) throws SQLException {
-				
-			}
-		};
-
-		RowMapper<User> rowMapper = new RowMapper<User>() {
-			
-			@Override
-			public User mapRow(ResultSet rs) throws SQLException {
-				return new User(rs.getString("userId"), 
-						rs.getString("password"), 
-						rs.getString("name"),
-						rs.getString("email"));
-			}
-		};
-		
+	public List<User> findAll() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "SELECT userId, password, name, email FROM USERS";
-		return (List<User>)jdbcTemplate.query(sql, pss, rowMapper);
-    }
 
-    public User findByUserId(String userId) throws SQLException {
-    	JdbcTemplate jdbcTemplate = new JdbcTemplate();
-    	
-    	PreparedStatementSetter pss = new PreparedStatementSetter() {
-			
-			@Override
-			public void setValues(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, userId);
-			}
-		};
-		
-		RowMapper<User> rowMapper = new RowMapper<User>() {
-			
+		RowMapper<User> rm = new RowMapper<User>() {
 			@Override
 			public User mapRow(ResultSet rs) throws SQLException {
-				return new User(rs.getString("userId"), 
-						rs.getString("password"), 
-						rs.getString("name"),
+				return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
 						rs.getString("email"));
 			}
 		};
-    	
-        String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-        return (User)jdbcTemplate.queryForObject(sql, pss, rowMapper);
-    }
+
+		return jdbcTemplate.query(sql, rm);
+	}
+
+	public User findByUserId(String userId) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+
+		RowMapper<User> rm = new RowMapper<User>() {
+			@Override
+			public User mapRow(ResultSet rs) throws SQLException {
+				return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+						rs.getString("email"));
+			}
+		};
+
+		return jdbcTemplate.queryForObject(sql, rm, userId);
+	}
 
 }
