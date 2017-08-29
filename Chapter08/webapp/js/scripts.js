de554ffdf0081118a69ff4b2783a5299481a33e0
Minseok Lee <lagender@gmail.com>
2017-08-29 23:45:12 +0900

    8.2.1 답변하기

---
+++ Chapter08/webapp/js/scripts.js
@@ -0,0 +1,37 @@
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+        ;
+  });
+};
+
+$(".answerWrite input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+	e.preventDefault();
+	var queryString = $("form[name=answer]").serialize();
+
+	$.ajax({
+		type : 'post',
+		url : '/api/qna/addAnswer',
+		data : queryString,
+		dataType : 'json',
+		error : onError,
+		success : onSuccess,
+	});
+}
+
+function onSuccess(json, status) {
+	var answerTemplate = $("#answerTemplate").html();
+	var template = answerTemplate.format(json.writer, new Date(json.createdDate), 
+			json.contents, json.answerId);
+	$(".qna-comment-slipp-articles").prepend(template);
+	
+}
+
+function onError(xhr, status) {
+	  alert("error");
+	}
